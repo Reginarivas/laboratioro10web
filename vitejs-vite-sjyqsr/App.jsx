4caf51840e76d97a6578077f681934e66ed14f2a
Reginarivas <110072961+Reginarivas@users.noreply.github.com>
2024-04-26 23:24:28 -0600

    Add files via upload

---
+++ vitejs-vite-sjyqsr/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import TaskInputForm from './TaskInputForm';
+import TaskList from './TaskList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const deleteTask = (task) => {
+    setTasks(tasks.filter((t) => t !== task));
+  };
+
+  return (
+    <div className="App container mt-5">
+      <h1>Mini Task Dashboard</h1>
+      <TaskInputForm addTask={addTask} />
+      <TaskList tasks={tasks} deleteTask={deleteTask} />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ vitejs-vite-sjyqsr/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+const Main = () => {
+  return (
+    <div>
+      <App />
+    </div>
+  );
+};
+
+ReactDOM.render(<Main />, document.getElementById('root'));
